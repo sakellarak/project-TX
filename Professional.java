bfce10dbe7650199c52056e7fe6a0b2a4d0ef749
Mike Sakell <sakellarak@gmail.com>
2020-05-31 18:19:01 +0300

    professional + user + mana

--- Professional.java
+++ Professional.java
@@ -1,4 +1,4 @@
-public class Professional {
+public class Professional extends User{
 
     String jobName;
     String jobDescription;
@@ -6,20 +6,61 @@ public class Professional {
     double range;
     String workingHours;
     double pricing;
-    String paymentMethod;
-    int photo; //LAMEO
+    String paymentMethod; //ανά ώρα ή ανά ραντεβού
+    boolean photo;
     double rating;
 
+    public Professional(String name, String password, int userID, String email, int telephone_number, String jobName, String jobDescription, String address, double range, String workingHours, double pricing, String paymentMethod, boolean photo, double rating){
+        super(name, password, userID, email, telephone_number);
+        this.jobName=jobName;
+        this.jobDescription=jobDescription;
+        this.address=address;
+        this.range=range;
+        this.workingHours=workingHours;
+        this.pricing=pricing;
+        this.paymentMethod=paymentMethod;
+        this.photo=photo;
+        this.rating=rating;
+    }
+
+    public void updateArea(String address, double range, String workingHours){
+
+        this.address=address;
+        this.range=range;
+        this.workingHours=workingHours;
+    }
+
+    public void updateDesc(String jobDescription, boolean photo){
+
+        this.jobDescription=jobDescription;
+        this.photo=photo;
+    }
+
+    public void updatePricing(double pricing, String paymentMethod){
+
+        this.pricing=pricing;
+        this.paymentMethod=paymentMethod;
+    }
+
+    public void getInfo() {
+        
+        System.out.println(name + telephone_number + jobDescription);
+    }
+
     public void inRange() {}
-    public void do1() {}
+    
+    public void do1() {}   //?????
+    
     public void addCritic() {}
+    
     
     /*
     public static void main(final String[] args) {
 
-        System.out.println("hello world");
-        int x=10;
-        System.out.println(x);
+        Professional pelaths = new Professional("Mixalhs","kwdikos",123,"kourhs@kourhs.com",12345,"giafka","perigrafh","dieuthynsh",10,"10-2",50,"ana radevou",true,4);
+        System.out.println(pelaths.jobDescription);
         
-    }*/
+        
+    }
+    */
 }
