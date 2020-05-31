7a98c4d31a260bc5a0b11cb27e540372b71f5e48
kourvalues <62709364+kourvalues@users.noreply.github.com>
2020-05-31 16:02:10 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,21 @@
+
+/**
+ * Superclass client kai pro den 8a exei instances
+ *
+ * @author kourhs
+ * 
+ */
+public class User
+{
+    public String name;
+    public String password;
+    public int userID;
+    public String email;
+    public int telephone_number;
+    
+    public void getInfo() {}
+    public void check() {}
+    public void update() {}
+    public void checkpass() {}
+    
+}
